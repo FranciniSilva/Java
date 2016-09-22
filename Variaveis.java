fcf39a203e612f18d91dec6c244d01dfa6faf251
FranciniSilva <franciniassis89@hotmail.com>
2016-09-21 22:02:11 -0300

    Convenções em Java

--- Variaveis.java
+++ Variaveis.java
@@ -6,7 +6,20 @@ public class Variaveis {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//convenção Java
+		int idade = 20;
+		String nome = "Francini";
+		String nomeDoMeuCachorro = "totó";
+		
+		idade = 25;
+		System.out.println("Idade = " + idade);
+		System.out.println("Nome = " + nome);
+		System.out.println("Nome do Cachorro = " + nomeDoMeuCachorro);
+		
+		//má prática
+		int a = 10;
+		String b = "Carol";
 
 	}
 
